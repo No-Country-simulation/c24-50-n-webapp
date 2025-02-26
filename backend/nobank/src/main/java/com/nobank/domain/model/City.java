9122e3b3c8f33ad0ed7ec0b6ab4487553ffe8265
walthersmith <wsfrancootero@gmail.com>
2025-02-25 22:58:25 -0500

    algunos ajustes en los modelos  City Country y State

--- backend/nobank/src/main/java/com/nobank/domain/model/City.java
+++ backend/nobank/src/main/java/com/nobank/domain/model/City.java
@@ -8,6 +8,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -31,4 +33,15 @@ public class City {
     private State state;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }   
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 }
--- backend/nobank/src/main/java/com/nobank/domain/model/State.java
+++ backend/nobank/src/main/java/com/nobank/domain/model/State.java
@@ -8,6 +8,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -25,11 +27,23 @@ public class State {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
-    private String code;
+    private String name; 
+
     @ManyToOne
     @JoinColumn(name = "country_id")
     private Country country;
+    
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    @PrePersist 
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 }
