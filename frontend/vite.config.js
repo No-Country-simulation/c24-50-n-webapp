a2ad6bac9c288ef7f6d1592a96afeb52ba4ed20d
LenzxDev <lvalda.dev@gmail.com>
2025-03-13 05:11:33 -0300

    conectar front-back

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [
     vue(),
@@ -16,6 +16,11 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  define: {
+    'process.env': {
+      VITE_API_BASE_URL: JSON.stringify(process.env.VITE_API_BASE_URL || 'http://localhost:8080'),
     },
   },
 })
